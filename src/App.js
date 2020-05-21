348902078d4624b8a684c518852c250eca5c92b7
salimsea <salimajah01@gmail.com>
2020-05-22 00:19:14 +0700

    add package svg and routing

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { GetStarted } from './pages';
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <GetStarted />
+    </NavigationContainer>
+  );
+};
+
+export default App;
